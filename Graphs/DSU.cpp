3f42276b957e67d02f70d3c8527bc906873c6800
Meet Joshi <meetjoshi2018@gmail.com>
2025-07-15 14:23:50 +0530

    DSU class rank and size

--- Graphs/DSU.cpp
+++ Graphs/DSU.cpp
@@ -3,22 +3,24 @@ using namespace std;
 
 class DSU {
     private:
-        vector<int> parent, rank;
+        vector<int> parent, rank, size;
     public:
         DSU(int n){
             rank.resize(n+1,0);
             parent.resize(n+1);
+            size.resize(n+1,0);
             for(int i =0;i<=n;i++){
                 parent[i]=i;
+                size(i)=1;
             }
         }
 
-        findUpar(int node){
+        int findUpar(int node){
             if(node == parent[node]) return node;
             return parent[node]=findUpar(parent[node])
         }
 
-        unionbyrank(int u, int v){
+        void unionbyrank(int u, int v){
             ulp_u = findUpar(u);
             ulp_v = findUpar(v);
             if(ulp_u == ulp_v) return;
@@ -33,4 +35,24 @@ class DSU {
                 rank[ulp_u]++;
             }
         }
+
+        void unionbysize(int u, int v){
+            ulp_u = findUpar(u);
+            ulp_v = findUpar(v);
+            if(ulp_u == ulp_v) return;
+            if(size[ulp_u] < size[ulp_v]){
+                parent[ulp_u] = ulp_v;
+                size[ulp_v] += size[ulp_u];
+            } 
+            else{
+                parent[ulp_v]=ulp_u;
+                size[ulp_u] += size[ulp_v];
+            }
+        }
+}
+
+int main(){
+    DSU ds(5);
+
+    return 0;
 }
